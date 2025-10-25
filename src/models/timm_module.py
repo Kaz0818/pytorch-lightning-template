accc03dcfd93068e9dc740436ef9242c533de2ba
Kaz0818 <kaznene.together.forever@gmail.com>
2025-10-25 16:04:24 +0900

    fix: matplotlib.pyplot as sns to plt

--- src/models/timm_module.py
+++ src/models/timm_module.py
@@ -8,7 +8,7 @@ import pytorch_lightning as pl
 import timm
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
-import matplotlib.pyplot as sns
+import matplotlib.pyplot as plt
 import seaborn as sns
 import wandb
 
